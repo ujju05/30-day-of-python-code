0347d0cf8b980183da7dc7ffa8c14a8d15d75171
ujju05 <97243972+ujju05@users.noreply.github.com>
2022-01-08 16:27:46 +0530

    variable

---
+++ day1/variable.py
@@ -0,0 +1,16 @@
+#add two number and print output
+a=int(input('enter the first value ='))
+b=int(input('enter the second value ='))
+print('total =',a+b)
+
+# add two strings and print output
+x=str(input('enter a 1st name or alphabet ='))
+y=str(input("enter 2nd name or alphabet"))
+print('your name is =',x + y)
+
+# add two floats and print output
+g=float(input('enter the first float value ='))
+h=float(input('enter the second float value ='))
+print('total =',g+h)
+
+

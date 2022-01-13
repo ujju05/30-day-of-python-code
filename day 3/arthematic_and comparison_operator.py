1335e63b27fff33baa88cbbf179ded5221bffca5
ujju05 <97243972+ujju05@users.noreply.github.com>
2022-01-13 22:28:23 +0530

    arthematic and comperison operator.py

---
+++ day 3/arthematic_and comparison_operator.py
@@ -0,0 +1,40 @@
+#programme for calculating modulus
+
+print('FOR MODULUS')
+a=int(input('enter the 1st number ='))
+b=int(input('enter the 2nd number ='))
+z=a%b
+print('reminder =',z)
+
+#progamme for calculating exponentiation
+
+print('FOR EXPONENTIATION')
+x=int(input('enter the 1st number ='))
+y=int(input('enter the 2nd number ='))
+q=x**y
+print('tatal =',q)
+
+#programme for calculating floor division
+
+print('FOR FLOOR DIVISION')
+m=int(input('enter the 1nd number ='))
+n=int(input('enter the 2nd number ='))
+i=m//n
+print('floor division =',i)
+
+#programe for comparison operator(TASK-2)
+
+print('TASK-2')
+A=int(input('Enter the value of A ='))
+B=int(input('enter the value of B ='))
+print('check less than(A<B)  =',A<B)
+
+print('check greater than(A>B) =',A>B)
+
+print('check less than(A<=B) =',A<=B)
+
+print('check greater than or equal to(A>=B) =',A>=B)
+
+print('equal to (A==B) =',A==B)
+
+print('not equal to (A!=B) =',A!=B)
